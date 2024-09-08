114a9f8385377df459a53d46265cd57d91e80552
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2024-09-08 23:45:59 +0900

    データの読取り、書込み時部分の共通化

--- sinatra_memo.rb
+++ sinatra_memo.rb
@@ -5,8 +5,16 @@ require 'sinatra/reloader'
 require 'json'
 require 'erb'
 
+def data_load(file_name)
+  File.open(file_name) { |file| JSON.parse(file.read) }
+end
+
+def data_write(file_name, memos)
+  File.open(file_name, 'w') { |file| JSON.dump(memos, file) }
+end
+
 get '/memos' do
-  @memos = File.open('memos.json') { |file| JSON.parse(file.read) }
+  @memos = data_load('memos.json')
   erb :memos_list
 end
 
@@ -15,7 +23,7 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  memos = File.open('memos.json') { |file| JSON.parse(file.read) }
+  memos = data_load('memos.json')
   @title = memos[params[:id]]['title']
   @body = memos[params[:id]]['body']
   erb :each_memo
@@ -25,20 +33,20 @@ post '/memos' do
   title = params[:title]
   body = params[:body]
 
-  memos = File.open('memos.json') { |file| JSON.parse(file.read) }
-  if memos == {}
-    id = 1
-  else
-    id = (memos.keys.map(&:to_i).max + 1).to_s
-  end
-    memos[id] = { 'title' => title, 'body' => body }
-  File.open('memos.json', 'w') { |file| JSON.dump(memos, file) }
+  memos = data_load('memos.json')
+  id = if memos == {}
+         1
+       else
+         (memos.keys.map(&:to_i).max + 1).to_s
+       end
+  memos[id] = { 'title' => title, 'body' => body }
+  data_write('memos.json', memos)
 
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  memos = File.open('memos.json') { |file| JSON.parse(file.read) }
+  memos = data_load('memos.json')
   @title = memos[params[:id]]['title']
   @body = memos[params[:id]]['body']
   erb :edit
@@ -48,17 +56,17 @@ patch '/memos/:id' do
   title = params[:title]
   body = params[:body]
 
-  memos = File.open('memos.json') { |file| JSON.parse(file.read) }
+  memos = data_load('memos.json')
   memos[params[:id]] = { 'title' => title, 'body' => body }
-  File.open('memos.json', 'w') { |file| JSON.dump(memos, file) }
+  data_write('memos.json', memos)
 
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = File.open('memos.json') { |file| JSON.parse(file.read) }
+  memos = data_load('memos.json')
   memos.delete(params[:id])
-  File.open('memos.json', 'w') { |file| JSON.dump(memos, file) }
+  data_write('memos.json', memos)
 
   redirect '/memos'
 end
