13b614d542bfd0650166fa7c7d75b0b5ac44d608
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2024-09-11 21:15:00 +0900

    メソッド名の修正

--- sinatra_memo.rb
+++ sinatra_memo.rb
@@ -5,16 +5,16 @@ require 'sinatra/reloader'
 require 'json'
 require 'erb'
 
-def load_memo(file_name)
+def load_memos(file_name)
   File.open(file_name) { |file| JSON.parse(file.read) }
 end
 
-def write_memo(file_name, memos)
+def write_memos(file_name, memos)
   File.open(file_name, 'w') { |file| JSON.dump(memos, file) }
 end
 
 get '/memos' do
-  @memos = load_memo('memos.json')
+  @memos = load_memos('memos.json')
   erb :memos_list
 end
 
@@ -23,7 +23,7 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  memos = load_memo('memos.json')
+  memos = load_memos('memos.json')
   @title = memos[params[:id]]['title']
   @body = memos[params[:id]]['body']
   erb :each_memo
@@ -33,20 +33,20 @@ post '/memos' do
   title = params[:title]
   body = params[:body]
 
-  memos = load_memo('memos.json')
+  memos = load_memos('memos.json')
   id = if memos == {}
          1
        else
          (memos.keys.map(&:to_i).max + 1).to_s
        end
   memos[id] = { 'title' => title, 'body' => body }
-  write_memo('memos.json', memos)
+  write_memos('memos.json', memos)
 
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  memos = load_memo('memos.json')
+  memos = load_memos('memos.json')
   @title = memos[params[:id]]['title']
   @body = memos[params[:id]]['body']
   erb :edit
@@ -56,17 +56,17 @@ patch '/memos/:id' do
   title = params[:title]
   body = params[:body]
 
-  memos = load_memo('memos.json')
+  memos = load_memos('memos.json')
   memos[params[:id]] = { 'title' => title, 'body' => body }
-  write_memo('memos.json', memos)
+  write_memos('memos.json', memos)
 
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = load_memo('memos.json')
+  memos = load_memos('memos.json')
   memos.delete(params[:id])
-  write_memo('memos.json', memos)
+  write_memos('memos.json', memos)
 
   redirect '/memos'
 end
